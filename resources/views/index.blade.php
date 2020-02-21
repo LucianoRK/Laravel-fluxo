1fcdcd21e52696a5011ea9822177011dd1dae08e
LucianoPRK <32466727+LucianoRK@users.noreply.github.com>
2020-02-20 22:40:44 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Projeto</title>
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="start"></div>
+    </body>
+    <script src="js/app.js"></script>
+</html>
