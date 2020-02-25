cc2b4beb1d956d5b13862798ed9368ec506a207b
Fernando <fernando.stavski@gmail.com>
2020-02-25 20:37:57 -0300

    Template Admin e login

--- public/js/app.js
+++ public/js/app.js
@@ -35112,8 +35112,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\www\projeto\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\www\projeto\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\projeto\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\projeto\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2> Dashboard </h2>
+    </div>
+@endsection
