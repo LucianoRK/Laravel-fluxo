70954d29a7f33ca11874e9d11384c25c5eadc810
Fernando <fernando.stavski@gmail.com>
2020-03-02 19:23:47 -0300

    Colocado template novamente

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+@section('title', 'Home')
+
+
+@section('content')
+    <p> Home do sistema </p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+//Route::get('/', 'UserController@index');
