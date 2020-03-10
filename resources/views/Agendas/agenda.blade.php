104f21ad16f9188a81a0dc1dcd09a92d7871b832
LucianoPRK <32466727+LucianoRK@users.noreply.github.com>
2020-03-09 23:46:23 -0300

    Iniciando agenda

--- resources/views/Agendas/agenda.blade.php
+++ resources/views/Agendas/agenda.blade.php
@@ -6,4 +6,67 @@
 
 @section('content')
     
+<div class="container">
+    <div class="row mb-2">
+        <div class="input-group col-8">
+            <span class="input-group-text" id="horario"> 08:00</span>
+            <input type="text" class="form-control" placeholder="Nome"  aria-describedby="horario">
+        </div>
+        <div class="col-4 form">
+            <button class="btn btn-info">EDITAR</button>
+            <button class="btn btn-danger">DELETAR</button>
+            <button class="btn btn-success">CADASTRAR</button>
+            <button class="btn btn-dark">NADA</button>
+        </div>
+    </div>
+    <div class="row mb-2">
+        <div class="input-group col-8">
+            <span class="input-group-text" id="horario"> 09:00</span>
+            <input type="text" class="form-control" placeholder="Nome"  aria-describedby="horario">
+        </div>
+        <div class="col-4 form">
+            <button class="btn btn-info">EDITAR</button>
+            <button class="btn btn-danger">DELETAR</button>
+            <button class="btn btn-success">CADASTRAR</button>
+            <button class="btn btn-dark">NADA</button>
+        </div>
+    </div>
+    <div class="row mb-2">
+        <div class="input-group col-8">
+            <span class="input-group-text" id="horario"> 10:00</span>
+            <input type="text" class="form-control" placeholder="Nome"  aria-describedby="horario">
+        </div>
+        <div class="col-4 form">
+            <button class="btn btn-info">EDITAR</button>
+            <button class="btn btn-danger">DELETAR</button>
+            <button class="btn btn-success">CADASTRAR</button>
+            <button class="btn btn-dark">NADA</button>
+        </div>
+    </div>
+    <div class="row mb-2">
+        <div class="input-group col-8">
+            <span class="input-group-text" id="horario">11:00</span>
+            <input type="text" class="form-control" placeholder="Nome"  aria-describedby="horario">
+        </div>
+        <div class="col-4 form">
+            <button class="btn btn-info">EDITAR</button>
+            <button class="btn btn-danger">DELETAR</button>
+            <button class="btn btn-success">CADASTRAR</button>
+            <button class="btn btn-dark">NADA</button>
+        </div>
+    </div>
+    <div class="row mb-2">
+        <div class="input-group col-8">
+            <span class="input-group-text" id="horario"> 12:00</span>
+            <input type="text" class="form-control" placeholder="Nome"  aria-describedby="horario">
+        </div>
+        <div class="col-4 form">
+            <button class="btn btn-info">EDITAR</button>
+            <button class="btn btn-danger">DELETAR</button>
+            <button class="btn btn-success">CADASTRAR</button>
+            <button class="btn btn-dark">NADA</button>
+        </div>
+    </div>
+</div>
+
 @endsection
